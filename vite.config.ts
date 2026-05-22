3d873ad96885e3c35bb514e1143782549714c120
Hiten Dharpure <hitendharpure01@gmail.com>
2026-05-22 23:14:57 +0530

    Set base path for portfolio deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/hiten-dharpure-portfolio/"
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
